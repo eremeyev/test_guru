ede9d5a27e9a77100c76099b567b466e1dedc67a
Andrey Eremeev <andrey.eremeyev@gmail.com>
2019-06-18 13:06:07 +0400

    add constant ANSWERS_LIMIT to answer model to avoid 'magic number'

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,8 +3,9 @@ class Answer < ApplicationRecord
   belongs_to :user
   scope :right, -> { where(correct: 'Yes') }
   validate :max_count
+  ANSWERS_LIMIT = 4
 
   def max_count
-    errors.add(:answers_count, "answers can't be more than 4 for one question") if question.answers.size >= 4
+    errors.add(:answers_count, "answers can't be more than 4 for one question") if question.answers.size >= ANSWERS_LIMIT
   end
 end
