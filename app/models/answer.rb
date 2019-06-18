1580d9752f8a2d24c7b9670233dea736f7e756d9
Andrey Eremeev <andrey.eremeyev@gmail.com>
2019-06-18 16:29:29 +0400

    refactor seeds, refactor test to be more clear, change validation on answer to call on create

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,10 +2,10 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   scope :right, -> { where(correct: true) }
-  validate :max_count
+  validate :max_count, on: :create
   ANSWERS_LIMIT = 4
 
   def max_count
-    errors.add(:answers_count, "answers can't be more than 4 for one question") if question.answers.size >= ANSWERS_LIMIT
+    errors.add(:answers_count, "answers can't be more than 4 for one question") if question && question.answers.size >= ANSWERS_LIMIT
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -4,13 +4,12 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
   validates :title, :level, presence: true
   validates :level, numericality: { only_integer: true,  greater_than_or_equal_to: 0 }
-  validates :level, :title, uniqueness: true
-  scope :simple, -> { where(level: 0..1) }
-  scope :middle, -> { where(level: 2..4) }
-  scope :hard,   -> { where(level: 5..Float::INFINITY) }
   scope :by_level, -> (level) { where(level: level) }
+  scope :simple, -> { by_level(0..1) }
+  scope :middle, -> { by_level(2..4) }
+  scope :hard,   -> { bylevel(5..Float::INFINITY) }
   
   def self.titles_by_category(category_title)
-    Test.joins(:category).where("categories.title = ?", category_title).pluck(:title)
+    Test.joins(:category).where("categories.title = ?", category_title).pluck(:title).sort
   end
 end
