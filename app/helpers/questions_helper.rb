72f2d22ca8093ac2c71560114e6590c81ef5997d
Andrey Eremeev <andrey.eremeyev@gmail.com>
2019-06-27 02:00:42 +0400

    all changes for lesson 'rails views'

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header(question)
+    content_tag :h2, 
+      "#{question.new_record? ? 'Create New' : 'Edit'} #{question.test.title} Question"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :tests do
     resources :questions
   end
+  
+  root to: 'tests#index'
    
 end
